47ddd249d0756d9a1ad1c591de68de90a7bbc0f4
Arewena <sjwj08@gmail.com>
2023-10-12 12:25:30 +0900

    :sparkles: learn InteractEvent and itemMeta

--- src/main/kotlin/io/github/arwena/pluginMain.kt
+++ src/main/kotlin/io/github/arwena/pluginMain.kt
@@ -2,9 +2,14 @@ package io.github.arwena
 
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 class pluginMain : JavaPlugin(), Listener { //Listener is needed when using EventHandler
@@ -23,6 +28,20 @@ class pluginMain : JavaPlugin(), Listener { //Listener is needed when using Even
         val sender = event.player
         event.joinMessage(text("${sender.name} joined game").color(NamedTextColor.AQUA))
 
+    }
+
+    @EventHandler
+    fun onRightClick(event: PlayerInteractEvent) {
+        val player = event.player
+        if (event.action == Action.RIGHT_CLICK_AIR && player.isSneaking) {
+            val item = ItemStack(Material.TOTEM_OF_UNDYING).apply {
+                itemMeta = itemMeta.apply {
+                    displayName(text("THE WORLD").color(NamedTextColor.GOLD))
+                }
+            }
+
+            player.inventory.addItem(item)
+        }
     }
 
 
