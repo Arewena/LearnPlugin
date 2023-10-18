9665c036be0ab00ce78802a211cf4fe33856ee46
Arewena <sjwj08@gmail.com>
2023-10-18 13:43:45 +0900

    :sparkles: Learn Scheduler and Some settings of EventHandler

--- src/main/kotlin/io/github/arwena/pluginMain.kt
+++ src/main/kotlin/io/github/arwena/pluginMain.kt
@@ -2,21 +2,30 @@ package io.github.arwena
 
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerItemBreakEvent
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerTeleportEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitScheduler
 
 class pluginMain : JavaPlugin(), Listener { //Listener is needed when using EventHandler
     override fun onEnable() {
         //Do Something
         server.pluginManager.registerEvents(this, this) //Essential to use EventHandler
     }
+
+
+
 
     override fun onDisable() {
         //Do Something
@@ -29,6 +38,20 @@ class pluginMain : JavaPlugin(), Listener { //Listener is needed when using Even
         event.joinMessage(text("${sender.name} joined game").color(NamedTextColor.AQUA))
 
     }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true) //priority = 우선도, ignoreCancelled = 핸들러가 취소된 이벤트를 무시하는지의 여부
+    fun onTeleport(event: PlayerTeleportEvent) {
+        if (!event.player.isOp) {
+            event.isCancelled = true
+
+            val scheduler = Bukkit.getScheduler().runTaskTimer(this,
+                Runnable {
+                    event.player.sendMessage("It is not able to Teleport!")
+                }, 10L, 0L)
+        }
+
+    }
+
 
     @EventHandler
     fun onRightClick(event: PlayerInteractEvent) {
