d5531ea70c22cf9794776f0f1cfed55cdf730e73
Arewena <sjwj08@gmail.com>
2023-10-23 12:21:28 +0900

    :sparkles: Learn how to add recipe

--- src/main/kotlin/io/github/arwena/pluginMain.kt
+++ src/main/kotlin/io/github/arwena/pluginMain.kt
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
@@ -15,13 +16,37 @@ import org.bukkit.event.player.PlayerItemBreakEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerTeleportEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.inventory.ShapelessRecipe
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitScheduler
 
 class pluginMain : JavaPlugin(), Listener { //Listener is needed when using EventHandler
     override fun onEnable() {
         //Do Something
+        setupRecipe()
         server.pluginManager.registerEvents(this, this) //Essential to use EventHandler
+    }
+
+
+    fun setupRecipe() {
+        //코드 참조: https://github.com/monun/series-survival/blob/master/src/main/kotlin/com/github/monun/survival/plugin/SurvivalPlugin.kt
+        server.addRecipe(
+            ShapedRecipe(NamespacedKey(this, "1"), ItemStack(Material.TOTEM_OF_UNDYING).apply {
+            editMeta {
+                it.displayName(text("THE WORLD"))
+                it.lore(listOf(text("궁극의 스탠드").color(NamedTextColor.LIGHT_PURPLE)))
+
+            }
+        }).apply {
+            shape("ENE",
+                  "NTN",
+                  "ENE")
+            setIngredient('E', Material.EMERALD)
+            setIngredient('T', Material.TOTEM_OF_UNDYING)
+            setIngredient('B', Material.NETHER_STAR)
+
+        })
     }
 
 
